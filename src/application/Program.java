0cfdf05868de0e4851f8eefa23b0fd7b631383ce
Celso Ricardo <capcelsoricardo@gmail.com>
2020-03-26 12:33:58 -0300

    Criação do Board e Pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Board;
+import boardgame.Position;
+
 public class Program {
 	
 	public static void main (String[] args) { 
-		System.out.println("Hello World!");
+				
+		Position pos = new Position(4, 5);
+		Board board = new Board(8, 8);
 	}
-
 }
